fcfc4a00763224bc0cc24dfac673a11528171fe2
João Paiva <jgpaiva@gsd.inesc-id.pt>
2013-11-05 20:20:00 +0000

    fix wpm log service restart

--- start.py
+++ start.py
@@ -15,7 +15,7 @@ def clean():
         os.system(command)
 
 def start():
-    command=" ".join(["pushd ~/radargun;./bin/benchmark.sh -i",str(numMachines),"`cat ~/slaves`;popd;pushd ~/wpm;./log_service.sh restart"])
+    command=" ".join(["grep -R 1099 ~/wpm/;cd ~/wpm;./log_service.sh stop;./log/clean.sh;cd ~/radargun;./bin/benchmark.sh -i",str(numMachines),"`cat ~/slaves`;cd ~/wpm;sleep 5;./log_service.sh start"])
     command='bash -c "'+command+'"'
     print command
     if not dryrun:
