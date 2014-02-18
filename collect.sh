a5c6943849b354d8a2dfa5ca622d7fd10b4edb32
João Paiva <jgpaiva@gsd.inesc-id.pt>
2014-02-18 15:18:57 +0000

    avoid overwriting previous results

--- collect.sh
+++ collect.sh
@@ -1,7 +1,16 @@
-for i in `cat ~/slaves`; do mkdir -p ../results/$i; scp $i:~/radargun/* ../results/$i; done
-cp -r ~/wpm/log/csv/ ../results/
-mkdir -p ../results/config
-cp -r wpm ../results/config
-cp -r radargun ../results/config
-cp -r beforeBenchmark.sh ../results/config
+FOLDERNAME="../results"
 
+if [ $FOLDERNAME ]; 
+then 
+    echo "folder $FOLDERNAME exists. copying to unique name!"; 
+    FOLDERNAME="../results".`date "+%Y%m%d.%H%M%S"`
+fi
+
+for i in `cat ~/slaves`; do mkdir -p $FOLDERNAME/$i; scp $i:~/radargun/* $FOLDERNAME/$i; done
+cp -r ~/wpm/log/csv/ $FOLDERNAME/
+mkdir -p $FOLDERNAME/config
+cp -r wpm $FOLDERNAME/config
+cp -r radargun $FOLDERNAME/config
+cp -r beforeBenchmark.sh $FOLDERNAME/config
+
+echo "copied results to $FOLDERNAME"
